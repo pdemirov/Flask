3e36bdc071e4873365a66ca833d0ae0a6a0c7368
pdemirov <krustachka@gmail.com>
2024-03-22 17:31:58 +0200

    added more routes with http status codes

--- flaskdemo/server.py
+++ flaskdemo/server.py
@@ -1,10 +1,8 @@
-from flask import Flask, jsonify
-
+from flask import Flask, jsonify, make_response
 
 app = Flask(__name__) #After importing the Flask module, create your Flask application by initializing the Flask class.
 
 @app.route("/") #You can now use the app you created in the previous task to create your first route.
-
 def index():  #Define the method for the main root URL.
     #return "hello world"  #Return the “Hello World” message to the client.
 
@@ -12,5 +10,16 @@ def index():  #Define the method for the main root URL.
     #    "1": 'hello',
     #    "2": 'world'
     #}
-
     return jsonify(message = 'hello world!!!') #jsonify 
+
+@app.route("/no_content")
+def no_content():
+    #return jsonify(message = "no content found"), 204
+    return {"message": "no content found"}, 204
+    
+
+@app.route('/exp')
+def index_explicit():
+    resp = make_response({"message": "response with make response ;)"})
+    resp.status_code = 200
+    return resp
